4254c0f6fc4ccce95b0af6a385a8fa0a3f6bc4a1
Andreas Humenberger <ahumenberger@gmail.com>
2019-03-22 20:08:48 +0100

    Update

--- src/AlgebraicDependencies.jl
+++ src/AlgebraicDependencies.jl
@@ -9,8 +9,7 @@ using Hecke
 using Nemo
 using LinearAlgebra
 using ContinuedFractions
-
-greet() = print("Hello World!")
+using Singular
 
 function dependencies(roots::Vector{Basic}; variables=Basic[])
     # println("Computing dependencies between ", roots)
@@ -23,8 +22,10 @@ function dependencies(roots::Vector{Basic}; variables=Basic[])
     # elseif length(variables) != ncols(lattice)
     #     throw("Number of variables does not match number of columms. Got $(length(variables)), need $(ncols(lattice))")
     # end
-    # return ideal(lattice, variables)
+    return ideal(lattice, variables)
 end
+
+is_rational(x::Basic) = SymEngine.BasicType(x) isa SymEngine.BasicType{Val{:Integer}} || SymEngine.BasicType(x) isa SymEngine.BasicType{Val{:Rational}}
 
 function minpoly(r::Basic, z::Symbol)
     s = Sym(z)
@@ -36,31 +37,43 @@ end
 
 function common_number_field(roots::Vector{Basic})
     v = :a
-    bs = Expr[]
+    minpolys = Basic[]
     ps = Polynomials.Poly[]
     for r in roots
-        bpoly, poly = minpoly(r, v)
-
-        push!(bs, convert(Expr, bpoly))
-        push!(ps, poly)
+        mp, p = minpoly(r, v)
+        if is_rational(r)
+            push!(minpolys, r)
+        else
+            push!(minpolys, mp)
+        end
+        push!(ps, p)
     end
+    indices = findall(is_rational, minpolys)
+    ratroots = [minpolys[i] for i in indices] 
+    deleteat!(minpolys, indices)
+    mps = [convert(Expr, b) for b in minpolys]
+    if isempty(mps)
+        return 1, roots, ps 
+    end
+
+    deleteat!(roots, indices)
     R, _ = PolynomialRing(QQ, string(v))
-    K, g = number_field(map(R, bs))
-    @info "" K
+    K, g = number_field(map(R, mps))
+    @debug "Number field" K
     S, mS = simple_extension(K)
+    @debug "Simple extension" S
     gs = [SymEngine.lambdify(Basic(replace(string(mS\(x)), "//"=>"/"))) for x in g]
-    @info "" gs
     rs = [f isa Function ? f(r) : Basic(f) for (f, r) in zip(gs, roots)]
-    K, rs, ps
+    for (i, ind) in enumerate(indices)
+        splice!(rs, ind:ind-1, ratroots[i])
+    end
+    Hecke.degree(S), rs, ps
 end
 
-function masser_bound(K, roots::Vector{Basic}, mpolys::Vector{Polynomials.Poly})
+function masser_bound(degree::Int, roots::Vector{Basic}, mpolys::Vector{Polynomials.Poly})
     k = length(roots)
     # assume all roots belong to the same number field
-#     @syms x
-#     # p = Poly(SymPy.minimal_polynomial(roots[1].theta, x), x)
-    d = Hecke.degree(K) #TODO: degree of field extension
-
+    d = degree
     # h = maximum height of the a[i]. The height of an algebraic number is the 
     # sum of the degree and the binary length of all coefficients in the 
     # defining equation over Q
@@ -72,9 +85,36 @@ function masser_bound(K, roots::Vector{Basic}, mpolys::Vector{Polynomials.Poly})
             h = h0
         end
     end
-    @info "Parameters for Masser's bound" d h k
+    @debug "Parameters for Masser bound" d h k
     return ceil(d^2 * (4*h*k*d* (log(2+d)/log(log(2+d)))^3)^(k-1) + 1)
 end
+
+function ideal(m::Matrix{BigInt}, x::Array{Basic,1})
+    if isempty(m)
+        return []
+    end
+
+    y = [Basic("v$i") for i in 1:length(x)]
+    base = Basic[]
+    for i in 1:nrows(m)
+        r = 1
+        for j in 1:ncols(m)
+            exp = m[i,j]
+            v = exp > 0 ? x[j] : y[j]
+            r *= v^abs(exp)
+        end
+        base = [base; r - 1]
+    end
+
+    inv = [xi*yi - 1 for (xi,yi) in zip(x,y)]
+    base = [base; inv]
+    
+    R, g = Singular.PolynomialRing(Singular.QQ, map(string, [y; x]))
+    sbasis = [R(convert(Expr, b)) for b in base]
+    I = Singular.Ideal(R, sbasis)
+    return Singular.eliminate(I, g[1:length(y)]...)
+end
+
 
 function findrelations(roots::Vector{Basic})
     # first treat zeros in the root list
@@ -89,27 +129,21 @@ function findrelations(roots::Vector{Basic})
     # end
 
     # TODO: common number field does not work as expected
-    K, roots, mpolys = common_number_field(roots) # TODO: does nothing if the roots belong to the same field
-    @info "Common number field" K roots mpolys
-    # println("Algebraic numbers: ", an)
-    # a = poly.(an)
-    # a = map(Hecke.minpoly, g)
-    # a = map(Basic ∘ string, a)
+    degree, roots, mpolys = common_number_field(roots) # TODO: does nothing if the roots belong to the same field
+    @debug "Common number field" degree roots mpolys
     rs = [convert(Complex{BigFloat}, x) for x in roots]
+    @debug "Complex roots" rs
 
-    # # println("Algebraic numbers (poly): ", a)
     relog = [real(log(x)) for x in rs]
     imlog = [imag(log(x)) for x in rs]
-    imlog = [imlog; 2*pi]
+    imlog = [imlog; 2*BigFloat(pi)]
 
-    @info "" relog imlog
-
-    # replace implicit zeros by explicit ones
+    # Replace implicit zeros by explicit ones
     for i in 1:length(rs)
         z = rs[i]
 
         # abs(z) == 1
-        if abs(abs(z) - 1) < 0.1 && simplify(abs(z)) == 1
+        if abs(abs(z) - 1) < 0.1 && abs(z) == 1
             relog[i] = 0
         end
 
@@ -120,14 +154,16 @@ function findrelations(roots::Vector{Basic})
             # TODO: try harder: If[ Element[RootReduce[z], Reals] && Element[Sqrt[RootReduce[z]], Reals], imLog[[i]] = 0 ];
         end
     end
+    @debug "reLog and imLog" relog imlog
 
-    # # comute a bound for the coefficients of the generators
-    bound = Int(masser_bound(K, roots, mpolys))
-    @info "Masser bound" bound
+
+    # Compute a bound for the coefficients of the generators
+    bound = Int(masser_bound(degree, roots, mpolys))
+    @debug "Masser bound" bound
 
     m = eye(Int, length(roots)) # identity matrix
 
-    # # successively refine the approximation until only valid generators are returned
+    # Successively refine the approximation until only valid generators are returned
     level = Int(ceil(log2(bound) + 1))
     while prod(Bool[check_relation(roots, m[i,:]) for i in 1:size(m)[1]]) == 0
         m1 = getbasis(relog, level, bound)
@@ -139,16 +175,20 @@ function findrelations(roots::Vector{Basic})
     return m
 end
 
+hnf_with_transform(m::Matrix{Int}) = Matrix{Int}.(Nemo.hnf_with_trafo(matrix(FlintZZ, m)))
+
+function lattice_divide(l::Matrix{Int}, d::Int)
+    n = ncols(l)
+    return z_module_intersect(l, d*eye(Int, n)) / d
+end
+
 function check_relation(a::Vector{Basic}, e::Vector{<:Integer})
     return SymEngine.expand(prod([ax^ex for (ax, ex) in zip(a,e)])) == 1
 end
 
 function convergent(x, n)
-    # TODO: is precision of 60 enough>
-    @info "" x n
     cf = ContinuedFraction(x)
     co = convergents(cf)
-    # res = iterate(co, n)[1]
     return iterate(co, n)[1]
 end
 
@@ -158,9 +198,8 @@ ncols(a::Matrix{<:Any}) = size(a)[2]
 function getbasis(l::Vector{BigFloat}, level::Int, bound::Int)
     n = length(l)
 
-    # first treat zero elements in l as special case
-    zpos = findall(x -> iszero(x), l)
-    # zpos = findzeros(l)
+    # First treat zero elements in l as special case
+    zpos = findall(iszero, l)
     if length(zpos) == length(l)
         return eye(Int, n)
     end
@@ -169,33 +208,25 @@ function getbasis(l::Vector{BigFloat}, level::Int, bound::Int)
         ll = deleteat!(copy(l), zpos)
         b = getbasis(ll, level, bound) # basis for nonzero numbers
         zvec = zeros(nrows(b), 1)
-        # insert new dimensions
+        # Insert new dimensions
         for pos in zpos
             b = hcat(b[:,1:pos-1], zvec, b[:,pos:end])
         end
-        # add unit vectors at the zero positions
+        # Add unit vectors at the zero positions
         b = vcat(b, eye(Int, n)[zpos,:])
         return Matrix{Int}(b)
     end
 
-    # println("========== Now for nonzero: $(l) | $(level) | $(bound)")
-
-    # now we assume that l is a list of nonzero real numbers
+    # Now we assume that l is a list of nonzero real numbers
     c1 = [convergent(x, level) for x in l]
     c2 = [convergent(x, level+1) for x in l]
 
-    # println("c1: ", c1)
-    # println("c2: ", c2)
-
     e = [1//denominator(x1*x2) for (x1, x2) in zip(c1, c2)]
-    # cfrac theorem says: | log[i] - c1[i] | <= e[i]
-
-    # refine the approximation such that all errors are smaller than the smallest
-    # element of l in absolute value *)
-
+    # Refine the approximation such that all errors are smaller than the smallest
+    # element of l in absolute value
     lev = level + 1
     while length(filter(x -> maximum(e) >= abs(x), c1)) > 0 && lev < level + 5
-        ex = findin(e, maximum(e)) # indices with greates error
+        ex = findall(x->x==maximum(e), e) # indices with greates error
 
         lev++
         for i in 1:length(ex)
@@ -207,15 +238,11 @@ function getbasis(l::Vector{BigFloat}, level::Int, bound::Int)
     end
 
     # now: max e[i] < min |c1[i]|
-
     # this bound guarantees that generators whose norm is at most bound will
     # appear in the LLL-reduced basis
     minc1 = minimum([abs(c) for c in c1])
     maxe = maximum([abs(c) for c in e])
-    # println("min: ", minc1)
-    # println("max: ", maxe)
     d = BigInt(ceil(2^((length(c1) - 1)/2)*bound/(minc1 - maxe)))
-    # println("Integer d: ", d)
     identity = eye(Int, n)
     row = c1 * d
     b = hcat(identity, row)
@@ -230,7 +257,6 @@ function getbasis(l::Vector{BigFloat}, level::Int, bound::Int)
             res = vcat(res, transpose(b[i,:]))
         end
     end
-    # b = filter(x -> ))
 
     # all remaining vectors are returned as candidates
     # TODO: result should be integer matrix?
@@ -246,7 +272,7 @@ function z_nullspace(matrix::Matrix{Int})
     zvec = zeros(size(h, 2))
 
     # TODO: find better way to filter zero vectors
-    res = Matrix{Int}(0, ncols(t))
+    res = Matrix{Int}(undef, 0, ncols(t))
     for i in 1:nrows(t)
         if iszero(h[i,:])
             res = vcat(res, transpose(t[i,:]))
@@ -269,7 +295,7 @@ function z_module_intersect(base1::Matrix{Int}, base2::Matrix{Int})
 
     m1 = transpose(base1)
     m2 = transpose(sol[:, 1:nrows(base1)])
-    return lll(transpose(m1 * m2))
+    return lll(collect(transpose(m1 * m2)))
 end
 
 function clear_denom(a::Matrix{Rational{BigInt}})
@@ -298,7 +324,17 @@ function (R::FmpqPolyRing)(p::Expr)
             $(p)
         end
     end
-    @info "" q
+    eval(q)
+end
+
+function (R::Singular.PolyRing)(p::Expr)
+    vs = gens(R)
+    vs = [:($(Symbol(string(v))) = $v) for v in vs]
+    q = quote
+        let $(vs...)
+            $(p)
+        end
+    end
     eval(q)
 end
 
